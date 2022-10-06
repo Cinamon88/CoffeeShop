2b74ac590a0c2454460cc7741d598e7f6aa6ad51
Lukasz Gorka <lukaszgorka@hotmail.com>
2022-10-06 16:38:57 +0200

    add initProduct, initContact, initAbout

--- src/js/settings.js
+++ src/js/settings.js
@@ -8,13 +8,25 @@ export const settings = {
 export const select = {
   templateOf: {
     contact: '#template-contact-page',
+    about: '#template-about-page',
   },
 
   containerOf: {
+    home: '#home-wrapper',
     contact: '#contact-wrapper',
-  }
+    pages: '#pages',
+  },
+
+  nav: {
+    links: '.header-nav a',
+  },
 };
 
+export const className = {
+  active: 'active',
+};
+  
 export const templates = {
-  contatc: Handlebars.compile(document.querySelector(select.templateOf.contact).innerHTML),
+  about: Handlebars.compile(document.querySelector(select.templateOf.about).innerHTML),
+  contact: Handlebars.compile(document.querySelector(select.templateOf.contact).innerHTML),
 };
