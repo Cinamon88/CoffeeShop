46fd2ff122acaea818cc35d2073116193cb2387d
Lukasz Gorka <lukaszgorka@hotmail.com>
2022-10-13 12:16:52 +0200

    add initWidgets to components

--- src/js/components/Contact.js
+++ src/js/components/Contact.js
@@ -1,10 +1,11 @@
-import { templates, select } from '../settings.js';
+import { templates, select, classNames } from '../settings.js';
 
 class Contact {
   constructor(element){
     const thisContact = this;
     
     thisContact.render(element);
+    thisContact.initWidgets();
   }
     
   render(element){
@@ -21,6 +22,28 @@ class Contact {
     thisContact.dom.links = thisContact.dom.wrapper.querySelectorAll(
       select.containerOf.contact
     );
+  }
+
+  initWidgets(){
+    const thisHome = this;
+
+    for(let link of thisHome.dom.links){
+      link.addEventListener('click', function(event){
+        event.preventDefault();
+
+        const hash = event.target.hash.replace('#', '');
+        const currentLink = document.querySelector('.header-nav a[href="#products"]');
+        const currentPageId = document.getElementById(select.containerOf.products);
+        const newPageId = document.getElementById(hash);
+        const newLink = document.querySelector(`.header-nav a[href="${event.target.hash}"]`);
+
+        window.location.hash = `#/${hash}`;
+        currentLink.classList.remove(classNames.nav.active);
+        currentPageId.classList.remove(classNames.nav.active);
+        newPageId.classList.add(classNames.pages.active);
+        newLink.classList.add(classNames.pages.active);
+      });
+    }
   }
 }
 
--- src/js/components/Home.js
+++ src/js/components/Home.js
@@ -1,10 +1,11 @@
-import { templates, select, } from '../settings.js';
+import { templates, select, classNames } from '../settings.js';
 
 class Home {
   constructor(element){
     const thisHome = this;
     
     thisHome.render(element);
+    thisHome.initWidgets();
     
   }
     
@@ -22,6 +23,28 @@ class Home {
     thisHome.dom.links = thisHome.dom.wrapper.querySelectorAll(
       select.containerOf.home
     );
+  }
+
+  initWidgets(){
+    const thisHome = this;
+
+    for(let link of thisHome.dom.links){
+      link.addEventListener('click', function(event){
+        event.preventDefault();
+
+        const hash = event.target.hash.replace('#', '');
+        const currentLink = document.querySelector('.header-nav a[href="#home"]');
+        const currentPageId = document.getElementById(select.containerOf.homeId);
+        const newPageId = document.getElementById(hash);
+        const newLink = document.querySelector(`.header-nav a[href="${event.target.hash}"]`);
+
+        window.location.hash = `#/${hash}`;
+        currentLink.classList.remove(classNames.nav.active);
+        currentPageId.classList.remove(classNames.nav.active);
+        newPageId.classList.add(classNames.pages.active);
+        newLink.classList.add(classNames.pages.active);
+      });
+    }
   }
 }
 
